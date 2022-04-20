d62d7210c901c073cabe80db3513f3c0910e5180
Wayne Wang <waynewang@ZHIQIANGs-MacBook-Pro-2.local>
2022-04-19 18:39:33 -0700

    added all the images

--- js/header.js
+++ js/header.js
@@ -1,31 +1,31 @@
 document.write(`
-    <header>
-        <img class="logo" src="images/logo.png" alt="Gimme Cards logo">
+<header>
+<img class="logo" src="images/logo.png" alt="Gimme Cards logo">
 
-        <a id="about" class="menu" href="about.html">
-            <h1>About Me</h1>
-        </a>
+<a href="about.html">
+    <h1 id="about" class="menu">About Me</h1>
+</a>
 
-        <a id="tutorial" class="menu" href="tutorial.html">
-            <h1>Tutorial</h1>
-        </a>
+<a href="tutorial.html">
+    <h1 id="tutorial" class="menu">Tutorial</h1>
+</a>
 
-        <a id="commands" class="menu" href="commands.html">
-            <h1>Commands</h1>
-        </a>
+<a href="commands.html">
+    <h1 id="commands" class="menu">Commands</h1>
+</a>
 
-        <a class="menu" href="tutorial.html">
-            <h1>Support</h1>
-        </a>
+<a href="tutorial.html">
+    <h1 class="menu">Support</h1>
+</a>
 
-        <a class="menu" href="tutorial.html">
-            <h1>Invite Me</h1>
-        </a>
-    </header>
+<a href="tutorial.html">
+    <h1 class="menu">Invite Me</h1>
+</a>
+</header>
 
-    <script>
-        function activate(id) {
-            document.getElementById(id).classList.add("menu-active");
-        }
-    </script>
+<script>
+function activate(id) {
+    document.getElementById(id).classList.add("menu-active");
+}
+</script>
 `);
