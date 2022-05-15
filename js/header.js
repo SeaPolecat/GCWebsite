97ce82bfc54101f4086e13dfa39e53da06ab8980
Wayne Wang <waynewang@Waynes-MacBook-Pro.local>
2022-05-14 20:45:03 -0700

    completed commands page

--- js/header.js
+++ js/header.js
@@ -14,11 +14,11 @@ document.write(`
     <h1 id="commands" class="menu">Commands</h1>
 </a>
 
-<a href="tutorial.html">
+<a href="https://discord.gg/X8DKPfWTr7" target="_blank">
     <h1 class="menu">Support</h1>
 </a>
 
-<a href="tutorial.html">
+<a href="https://discord.com/oauth2/authorize?client_id=814025499381727232&permissions=272448&scope=bot" target="_blank">
     <h1 class="menu">Invite Me</h1>
 </a>
 </header>
