7d015e1ca95d7c33ec8a603d499050b59af71c94
Wayne Wang <waynewang@Waynes-MacBook-Pro.local>
2022-05-16 22:27:24 -0700

    completed rough draft of all pages

--- js/header.js
+++ js/header.js
@@ -1,26 +1,24 @@
 document.write(`
 <header>
-<img class="logo" src="images/logo.png" alt="Gimme Cards logo">
+<img class="logo" src="images/logo.png" alt="logo">
 
-<a href="about.html">
-    <h1 id="about" class="menu">About Me</h1>
-</a>
-
-<a href="tutorial.html">
-    <h1 id="tutorial" class="menu">Tutorial</h1>
-</a>
-
-<a href="commands.html">
-    <h1 id="commands" class="menu">Commands</h1>
-</a>
-
-<a href="https://discord.gg/X8DKPfWTr7" target="_blank">
-    <h1 class="menu">Support</h1>
-</a>
-
-<a href="https://discord.com/oauth2/authorize?client_id=814025499381727232&permissions=272448&scope=bot" target="_blank">
-    <h1 class="menu">Invite Me</h1>
-</a>
+<div class="top-menu">
+    <a href="about.html">
+        <h1 id="about" class="top-menu">About Me</h1>
+    </a>
+    <a href="tutorial.html">
+        <h1 id="tutorial" class="top-menu">Tutorial</h1>
+    </a>
+    <a href="commands.html">
+        <h1 id="commands" class="top-menu">Commands</h1>
+    </a>
+    <a href="https://discord.gg/X8DKPfWTr7" target="_blank">
+        <h1 class="top-menu">Support</h1>
+    </a>
+    <a href="https://discord.com/oauth2/authorize?client_id=814025499381727232&permissions=272448&scope=bot" target="_blank">
+        <h1 class="top-menu">Invite Me</h1>
+    </a>
+</div>
 </header>
 
 <script>
