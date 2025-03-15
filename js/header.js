d01fb73e71327b488472e7c1b89c5721a0e9bc8d
Cyberia <76026812+Cybelyrium@users.noreply.github.com>
2025-03-14 22:59:36 -0300

    remove premium button

--- js/header.js
+++ js/header.js
@@ -18,11 +18,6 @@ document.write(`
     </a>
     <h1 class="top-menu">|</h1>
 
-    <a href="https://ko-fi.com/gimmecards" target="_blank">
-        <h1 class="top-menu">Premium</h1>
-    </a>
-    <h1 class="top-menu">|</h1>
-
     <a href="https://discord.gg/wmVvK2cyzM" target="_blank">
         <h1 class="top-menu">Play Now</h1>
     </a>
